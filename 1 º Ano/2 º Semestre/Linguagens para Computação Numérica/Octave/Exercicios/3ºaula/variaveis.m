c859809304c4e4ad22a8d3799027019cd8fd1ce0
Luis Miguel Pereira Silva <nucleo.fisica.um@gmail.com>
2022-09-15 22:29:45 +0100

    added

---
+++ 1 º Ano/2 º Semestre/Linguagens para Computação Numérica/Octave/Exercicios/3ºaula/variaveis.m
@@ -0,0 +1,96 @@
+# Created by Octave 6.2.0, Fri Mar 19 10:40:18 2021 GMT <unknown@LAPTOP-7K2OMTO0>
+# name: A
+# type: matrix
+# rows: 2
+# columns: 2
+ 1 2
+ 3 4
+
+
+# name: B
+# type: matrix
+# rows: 2
+# columns: 2
+ 1 -2
+ -1 0
+
+
+# name: C
+# type: matrix
+# rows: 2
+# columns: 2
+ 2 -2
+ -2 2
+
+
+# name: I
+# type: diagonal matrix
+# rows: 2
+# columns: 2
+1
+1
+
+
+# name: M
+# type: matrix
+# rows: 3
+# columns: 3
+ 8 1 6
+ 3 5 7
+ 4 9 2
+
+
+# name: N
+# type: matrix
+# rows: 4
+# columns: 4
+ 16 2 3 13
+ 5 11 10 8
+ 9 7 6 12
+ 4 14 15 1
+
+
+# name: O
+# type: matrix
+# rows: 2
+# columns: 2
+ 0 0
+ 0 0
+
+
+# name: S
+# type: matrix
+# rows: 3
+# columns: 3
+ 1 2 3
+ 4 5 6
+ 7 8 9
+
+
+# name: a
+# type: scalar
+10
+
+
+# name: ans
+# type: matrix
+# rows: 1
+# columns: 4
+ 1 6 8 9
+
+
+# name: facil
+# type: matrix
+# rows: 9
+# columns: 9
+ 0 7 2 0 0 1 8 0 5
+ 0 5 1 0 3 7 0 9 0
+ 4 0 0 2 0 8 1 0 7
+ 0 4 7 5 2 0 3 0 0
+ 0 2 6 7 0 0 5 0 1
+ 5 0 0 1 0 6 0 2 9
+ 2 9 0 3 7 0 0 1 0
+ 7 0 0 0 6 2 0 5 3
+ 3 0 8 0 1 0 2 7 0
+
+
